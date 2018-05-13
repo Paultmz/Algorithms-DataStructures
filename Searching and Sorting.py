77b7421a0db287c3616222ae698b1d37aa779f45
Paultmz <paul.tang.git@gmail.com>
2018-05-13 15:25:48 +0800

    Added bubblesort and mergesort

--- Searching and Sorting.py
+++ Searching and Sorting.py
@@ -21,6 +21,49 @@ def binSearch(arr, x):
             return mid
     return -1
 
+# Bubblesort
+def bubbleSort(arr):
+    ans = arr[:]
+    flag = True
+    while flag:
+        flag = False
+        for i in range(1,len(ans)):
+            if ans[i]<ans[i-1]:
+                ans[i],ans[i-1] = ans[i-1],ans[i]
+                flag = True
+    return ans
+
+# Mergesort        
+def mergeSort(arr):
+    if len(arr)<=1:
+        return arr
+    mid = int(len(arr)/2)
+    left = mergeSort(arr[:mid])
+    right = mergeSort(arr[mid:])
+    return merge(left,right)
+
+def merge(arr1,arr2):
+    ans = []
+    i,j = 0,0
+    while i<len(arr1) and j<len(arr2):
+        if arr1[i]<arr2[j]:
+            ans.append(arr1[i])
+            i += 1
+        else:
+            ans.append(arr2[j])
+            j += 1
+    ans += arr1[i:] if i<len(arr1) else arr2[j:]
+    return ans
+
+
+
+# Testcase
 arr = [1,2,3,5,6,7,8,10,13]
 print(linSearch(arr,10))
 print(binSearch(arr,10))
+
+arr = [1,5,3,2,4,11,8,6,9]
+arr2 = bubbleSort(arr)
+arr3 = mergeSort(arr)
+print(arr2)
+print(arr3)
